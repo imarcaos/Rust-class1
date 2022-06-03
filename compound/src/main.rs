e1b8d5b96cb5c62cabd09561a42cb0392a6f0657
Marcos Melo <marcaos.melo@gmail.com>
2022-06-03 13:26:22 +0100

    Arrays Test

---
+++ compound/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // test with arrays
+
+    /* let array = [1u32, 2, 3];
+    let first_element = array[0];
+    let element = array[100];
+
+    let len = "Some text".len();
+    [1][len];
+
+    let tuple = (1u32, 2, true);
+    let first_element = tuple.0;
+    let element = tuple.100;*/
+}
