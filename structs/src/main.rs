1a89ea016c1c88d4b8410faa7d0b9d5d2427ee32
Marcos Melo <marcaos.melo@gmail.com>
2022-06-06 16:11:34 +0100

    Structs
    rust class - structs ex2

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,6 +4,8 @@ struct Person {
     likes_oranges: bool
 }
 
+struct Point2D(u32, u32);
+
 fn main() {
     let person = Person {
         name: String::from("Adam"),
@@ -12,4 +14,12 @@ fn main() {
     };
 
     println!("Person name is: {}", person.name);
+
+    let origin = Point2D(100, 200);
+
+    println!("Point contains {:?} and {:?}", origin.0, origin.1);
+
+    let Point2D(x, y) = origin;
+
+    println!("Point contains {:?} and {:?}", x, y);
 }
